50af88050e6dc96ca46e585a10c01ccc2709d91d
Rinat Gabidullin <gabidullin_rg@mail.ru>
2020-02-15 15:23:10 +0500

    Step 4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const whatIsYourName = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log (`Hello, ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 };
 
 export default whatIsYourName;
