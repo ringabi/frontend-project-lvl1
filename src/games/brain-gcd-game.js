fefff331a0c105535c71fd833689295f7a71e3be
Rinat Gabidullin <gabidullin_rg@mail.ru>
2020-03-01 19:13:09 +0500

    To check 3

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -3,6 +3,15 @@ import getRandomNumber from '../utils.js';
 
 const task = 'Find the greatest common divisor of given numbers.';
 
+const getAnswer = (minNumber, maxNumber) => {
+  let answer;
+  for (answer = minNumber; answer > 0; answer -= 1) {
+    if (minNumber % answer === 0 && maxNumber % answer === 0) {
+      return answer;
+    }
+  } return answer;
+};
+
 const getGreatestCommonDivisor = () => {
   const pairs = [];
   for (let attemptNumber = 0; attemptNumber < numberEngineAttempts; attemptNumber += 1) {
@@ -11,12 +20,7 @@ const getGreatestCommonDivisor = () => {
     const number2 = getRandomNumber();
     const maxNumber = (number1 > number2) ? number1 : number2;
     const minNumber = (number1 > number2) ? number2 : number1;
-    let answer;
-    for (answer = minNumber; answer > 0; answer -= 1) {
-      if (minNumber % answer === 0 && maxNumber % answer === 0) {
-        break;
-      }
-    }
+    const answer = getAnswer(minNumber, maxNumber);
     const question = [` ${number1}  ${number2}`];
     pair.push(question);
     pair.push(answer);
--- src/games/brain-prime-game.js
+++ src/games/brain-prime-game.js
@@ -3,17 +3,21 @@ import getRandomNumber from '../utils.js';
 
 const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+const getAnswer = (question) => {
+  let answer = 'yes';
+  for (let i = question - 1; i > 1; i -= 1) {
+    if (question % i === 0) {
+      answer = 'no';
+    }
+  } return answer;
+};
+
 const isAPrime = () => {
   const pairs = [];
   for (let attemptNumber = 0; attemptNumber < numberEngineAttempts; attemptNumber += 1) {
     const pair = [];
     const question = getRandomNumber();
-    let answer = 'yes';
-    for (let i = question - 1; i > 1; i -= 1) {
-      if (question % i === 0) {
-        answer = 'no';
-      }
-    }
+    const answer = getAnswer(question);
     pair.push(question);
     pair.push(answer);
     pairs.push(pair);
